b178263748d43e743684ee6f418368f80ed81180
piersonlinde <plinde1@gmail.com>
2024-09-26 20:59:57 -0600

    Fixed Map

--- weather/weather.js
+++ weather/weather.js
@@ -1,6 +1,9 @@
 var map = L.map('weathermap').setView([38, -95], 4);
 var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'; 
-var basemap = L.tileLayer(basemapUrl, {attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}) 
+var basemap = L.tileLayer(basemapUrl, 
+	{attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'	
+	}).addTo(map);
+
 
 var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
 var radarDisplayOptions = {
