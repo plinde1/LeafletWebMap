8a486729e88aeb1a91930feef1b5e88d60b02b44
piersonlinde <plinde1@gmail.com>
2024-09-26 20:31:13 -0600

    Code Cleanup

--- weather/weather.js
+++ weather/weather.js
@@ -1,27 +1,24 @@
 var map = L.map('weathermap').setView([38, -95], 4);
 var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'; 
-var basemap = L.tileLayer(basemapUrl, {attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map); 
+var basemap = L.tileLayer(basemapUrl, {attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}) 
 
 var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
 var radarDisplayOptions = {
     layers: 'nexrad-n0r-900913',
-    format: 'image/png'
+    format: 'image/png',
+	transparent: true
 };
 
 var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
 
-//https is different in pdf if this doesn't work
-var weatherAlertsUrl = 'https://api.weather.gov/alerts';
-//Remember getJSON could go her but it is geoJSON in the video
+
+var weatherAlertsUrl = ' https://www.weather.gov/documentation/services-web-api#/default/get_alerts_active';
 $.getJSON(weatherAlertsUrl, function(data) {
 
     L.geoJSON(data, {
-        style: function(feature) {
+        style: function(feature){
             var alertColor = 'orange';
-            if (feature.properties.severity === 'Severe') { 
-                alertColor = 'red';
-            }
+            if (feature.properties.severity === 'Severe') alertColor = 'red';
             return { color: alertColor };
         },
         onEachFeature: function(feature, layer) { 
