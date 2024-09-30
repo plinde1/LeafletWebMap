de2b21413bf9c9e4f939723da14c678748e69148
piersonlinde <plinde1@gmail.com>
2024-09-29 21:10:07 -0600

    Changing base and adding alerts

--- weather/weather.js
+++ weather/weather.js
@@ -1,5 +1,5 @@
 var map = L.map('weathermap').setView([38, -95], 4);
-var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'; 
+var basemapUrl = 'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'; 
 var basemap = L.tileLayer(basemapUrl, 
 	{attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'	
 	}).addTo(map);
@@ -15,13 +15,35 @@ var radarDisplayOptions = {
 var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
 
 
-var weatherAlertsUrl = ' https://api.weather.gov/alert/active?region_type=land';
+var weatherAlertsUrl = 'https://www.weather.gov/documentation/services-web-api#/default/get_alerts_active)';
 $.getJSON(weatherAlertsUrl, function(data) {
 
     L.geoJSON(data, {
         style: function(feature){
             var alertColor = 'orange';
-            if (feature.properties.severity === 'Severe') alertColor = 'red';
+            if (feature.properties.severity === 'Severe') alertColor = 'orange';
+            return { color: alertColor };
+        },
+        onEachFeature: function(feature, layer) { 
+            layer.bindPopup(feature.properties.headline);
+        }
+    }).addTo(map);
+
+    L.geoJSON(data, {
+        style: function(feature){
+            var alertColor = 'red';
+            if (feature.properties.severity === 'Extreme') alertColor = 'red';
+            return { color: alertColor };
+        },
+        onEachFeature: function(feature, layer) { 
+            layer.bindPopup(feature.properties.headline);
+        }
+    }).addTo(map);
+
+    L.geoJSON(data, {
+        style: function(feature){
+            var alertColor = 'green';
+            if (feature.properties.severity === 'Minor') alertColor = 'green';
             return { color: alertColor };
         },
         onEachFeature: function(feature, layer) { 
